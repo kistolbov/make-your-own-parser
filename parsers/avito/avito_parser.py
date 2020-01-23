3a5847aaf1dcdcd1cbc1570dfe14088b89775d33
Kirill Stolbov <TeaAntinomy@yandex.ru>
2020-01-23 15:30:26 +0500

    import libraries

---
+++ parsers/avito/avito_parser.py
@@ -0,0 +1,14 @@
+import csv
+import random
+import re
+import requests
+import time
+from collections import namedtuple
+
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+
+class AvitoParser():
+    def __init__(self):
+        pass
