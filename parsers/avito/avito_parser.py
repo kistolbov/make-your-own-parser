3991d4092e3838653d094e2e2710ec0cecea44a0
Kirill Stolbov <TeaAntinomy@yandex.ru>
2020-01-27 14:30:48 +0500

    make a new structure

--- parsers/avito/avito_parser.py
+++ parsers/avito/avito_parser.py
@@ -9,6 +9,55 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 
-class AvitoParser():
-    def __init__(self):
+class AvitoParser:
+    def __init__(self, page, location, search_query):
+        self.page = page
+        self.location = location
+        self.search_query = search_query
+
+    def create_keywords(self):
+        keywords = {
+            'page': self.page,
+            'location': self.location,
+            'search_query': self.search_query
+        }
+
+        return keywords
+
+    def get_desktop_html(self):
         pass
+
+    def get_mobile_html(self):
+        pass
+
+    def get_soup(self):
+        pass
+
+    def get_link(self):
+        pass
+
+    def get_data_from_link(self):
+        pass
+
+    def get_address(self):
+        pass
+
+    def get_price(self):
+        pass
+
+    def get_phone_number(self):
+        pass
+
+    def get_links_from_csv(self):
+        pass
+
+    def write_data_to_csv(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    pass
